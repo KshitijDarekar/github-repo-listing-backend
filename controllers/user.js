5b9cd4d52aa99dc24f6289f24a8158a5195abdbf
Kshitij Darekar <kshitijdarekar13@gmail.com>
2023-01-14 12:53:04 +0530

    feat: route to fetch user repositories
     getUserRepositories - function to fetch user repositories from Github API

--- controllers/user.js
+++ controllers/user.js
@@ -1,14 +1,14 @@
 import fetch from "node-fetch";
 
 // URL for the Github API
-const URL="https://api.github.com/users/";
+const URL = "https://api.github.com/users/";
 
 /**
  * getUserData - function to fetch user data from Github API
  *
  * @param {username} req - Express request object
  * @param {Object} res - Express response object
- *
+ * @route - GET /api/user
  * @returns {Object} - JSON object containing the user data from github
  */
 
@@ -26,8 +26,30 @@ const getUserData = async (req, res) => {
     return res.status(200).json(json);
   } catch (error) {
     // If there is an error, send error message in the response
-    return res.status(404).json(error);
+    return res.status(404).json(error.message);
   }
 };
 
-export { getUserData };
+/**
+ * getUserRepositories - function to fetch user repositories from Github API
+ *
+ * @param {username,per_page,page} req - Express request object
+ * @param {Object} res - Express response object
+ * @route - GET /api/repos
+ * @returns {Object} - JSON object containing the user repositories
+ */
+const getUserRepositories = async (req, res) => {
+  try {
+    // Fetch user repositories from Github API
+    const response = await fetch(
+      `${URL}${req.query.username}/repos?per_page=${req.query.per_page}&page=${req.query.page}`
+    );
+    const repositories = await response.json();
+    // Send repositories in the response
+    return res.status(200).json(repositories);
+  } catch (error) {
+    return res.status(404).json(error.message);
+  }
+};
+
+export { getUserData, getUserRepositories };
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -2,9 +2,9 @@ import express from "express";
 
 const router = express.Router();
 
-import { getUserData } from "../controllers/user.js";
+import { getUserData, getUserRepositories } from "../controllers/user.js";
 
 router.get("/user", getUserData);
-
+router.get("/repos", getUserRepositories);
 
 export default router;
